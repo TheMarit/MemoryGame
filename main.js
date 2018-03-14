46ec0bbc9b45c8cf304bdb713ae9656a057e5651
Marit Oliemans <maritoliemans@hotmail.com>
2018-03-14 16:24:33 -0600

    Add icons randomly on page load

---
+++ main.js
@@ -0,0 +1,32 @@
+let memory = {
+	currentFlip: [],
+	icons: ["plane", "coffee", "beer", "bicycle", "umbrella", "heart", "bolt", "tree", "plane", "coffee", "beer", "bicycle", "umbrella", "heart", "bolt", "tree"]
+}
+
+Array.prototype.shuffle = function() {
+    let input = this;
+    for (let i = input.length-1; i >=0; i--) {
+        let randomIndex = Math.floor(Math.random()*(i+1)); 
+        let itemAtIndex = input[randomIndex]; 
+        input[randomIndex] = input[i]; 
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
+
+$(document).ready(loadIcons(memory.icons));
+
+function loadIcons(arr){
+	arr.shuffle();
+	for (let icon of arr){
+		$("#game").append(
+			`<div class="cardcontainer">
+				<div class="card ${icon}">
+					<div class="front"></div>
+					<div class="back">
+						<i class="fas fa-${icon}"></i>
+					</div>
+				</div>
+			</div>`)
+	}
+}
